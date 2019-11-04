ff1a886e8c6818bf691525f6929dd650aa50d6c0
rogeriop <pinheiroxrogerio@gmail.com>
2019-11-04 10:52:12 -0300

    Curso devReactJs #09.01: Semana 5 - Criando o projeto e setando o Firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBDD5cjQgQzCHaegvu2zfSH7CBhisSlmVE",
+    authDomain: "comentaki-rogeriop.firebaseapp.com",
+    databaseURL: "https://comentaki-rogeriop.firebaseio.com",
+    projectId: "comentaki-rogeriop",
+    storageBucket: "comentaki-rogeriop.appspot.com",
+    messagingSenderId: "898728427479",
+    appId: "1:898728427479:web:08da77a108c37861cb1592"
+  };
+  firebase.initializeApp(firebaseConfig)
+
+  export default firebase
