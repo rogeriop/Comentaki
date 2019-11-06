92f8757bcf397191d818722cd9c27cba1c127a40
rogeriop <pinheiroxrogerio@gmail.com>
2019-11-06 16:37:04 -0300

    Curso devReactJs #09.03: Semana 5 - custom hook para carregar dados do Firebase

--- src/App.js
+++ src/App.js
@@ -2,32 +2,42 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import firebase from './firebase'
 
-const Comments = () => {
+const useDatabase = endpoint => {
   const [data, setData] = useState({})
   useEffect(() => {
-    const ref =  firebase.database().ref('test')
+    const ref =  firebase.database().ref(endpoint)
     ref.on('value', snapshot => {
       setData(snapshot.val())
     })
     return () => {
       ref.off() // desconecta do Firebase
     }
-  }, [])
+  }, [endpoint])
+  return { data }
+}
+
+//
+const Comments = ({visible}) => {
+  const endpoint = visible ? 'test' : 'test/a'
+  const data = useDatabase(endpoint)
   return (
     <pre>{JSON.stringify(data)}</pre>
   )
 }
 
+const  A = () => {
+  const data = useDatabase('test/a')
+  return (<pre>{JSON.stringify(data)}</pre>)
+}
+
 function App() {
   const [visible, toggle] = useState(true)
-
-
-
 
   return (
     <div>
     <button onClick={() => toggle(!visible)}>Toggle</button>
-    { visible && <Comments /> }
+    <Comments visible={visible} />
+    <A />
     </div>
   );
 }
