e8c54ccd4fae67e8b0258b7754daa7ed11df6f24
rogeriop <pinheiroxrogerio@gmail.com>
2019-11-06 09:18:00 -0300

    Curso devReactJs #09.02: Semana 5 - useEffect para limpar a conexão com o Firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import firebase from './firebase'
 
-const ref =  firebase.database().ref('test')
-ref.on('value', snapshot => {
-  console.log(snapshot.val())
-})
+const Comments = () => {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    const ref =  firebase.database().ref('test')
+    ref.on('value', snapshot => {
+      setData(snapshot.val())
+    })
+    return () => {
+      ref.off() // desconecta do Firebase
+    }
+  }, [])
+  return (
+    <pre>{JSON.stringify(data)}</pre>
+  )
+}
 
 function App() {
+  const [visible, toggle] = useState(true)
+
+
+
+
   return (
     <div>
-
+    <button onClick={() => toggle(!visible)}>Toggle</button>
+    { visible && <Comments /> }
     </div>
   );
 }
