989c1a01e6a3559127223248522600d6e53fe947
rogeriop <pinheiroxrogerio@gmail.com>
2019-11-08 16:09:00 -0300

    Curso devReactJs #09.06: Semana 5 - Criando novo comentário

--- src/App.js
+++ src/App.js
@@ -32,11 +32,22 @@ const useDatabasePush = endpoint => {
   return [status, save]
 }
 
+const Time = ({ timestamp }) => {
+  const date = new Date(timestamp)
+  const hours = date.getHours()
+  const minutes = '0'+date.getMinutes()
+  const seconds = '0'+date.getSeconds()
+  const day = '0'+(date.getDay()+1)
+  const month = '0'+(date.getMonth()+1)
+  const year = date.getFullYear()
+
+  return `${day.substr(-2)}/${month.substr(-2)}/${year} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`
+}
 //
 const Comment = ({ comment }) => {
   return (
     <div>
-      {comment.content} por: {comment.user.name}
+      {comment.content} por: {comment.user.name} em: <Time timestamp={comment.createdAt} />
     </div>
   )
 }
@@ -54,20 +65,35 @@ const Comments = () => {
   })
 }
 
-function App() {
+const NewComment = props => {
   const [, save] = useDatabasePush('comments')
-
-  return (
-    <div>
-    <button onClick={() => {
+  const [comment, setComment] = useState('')
+  const createComment = () => {
+    if (comment != '') {
       save({ 
-        content: 'olá aqui é meu comentário',
+        content: comment,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
         user: {
           id: '1',
           name: 'Rogério'
         } 
       })
-    }}>Toggle</button>
+      setComment('')
+    }
+  }
+  return (
+    <div>
+      <textarea value={comment} onChange={evt => setComment(evt.target.value)} />
+      <button onClick={createComment}>Comentar</button>
+    </div>
+  )
+}
+
+function App() {
+
+  return (
+    <div>
+    <NewComment />
     <Comments />
     </div>
   );
