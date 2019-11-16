fed038a94d2ef86f97bd5166a5c2780e39cff47f
rogeriop <pinheiroxrogerio@gmail.com>
2019-11-16 00:33:22 -0300

    Curso devReactJs #09.10: Semana 5 - Alterando o displayName do usuário

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import NewComment from './NewComment'
 import Comments from './Comments'
 
 import { AuthProvider } from './auth'
 import CreateUser from './CreateUser'
+import UserInfo from './UserInfo'
 
-//
-/*
-// Criar conta e autenticar
-firebase
-  .auth()
-  .createUserWithEmailAndPassword('pinheiro@gmail.com', 'abc123')
-  .then( user => {
-    console.log(user)
-  })
- * / 
-// Altera dados da conta
 
-*/
 
 function App() {
-
   return (
     <AuthProvider>
       <div>
         <NewComment />
         <Comments />
         <CreateUser />
+        <UserInfo />
       </div>
     </AuthProvider>
   );
--- src/NewComment.js
+++ src/NewComment.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { useDatabasePush } from './database'
 import firebase from './firebase'
+import { AuthContext } from './auth'
 
-const NewComment = props => {
+const NewComment = () => {
     const [, save] = useDatabasePush('comments')
     const [comment, setComment] = useState('')
+    const auth = useContext(AuthContext)
+    if (auth.user === null) {
+      return null
+    }
+    const { displayName } = auth.user
+    const [alternativeDisplayName] = auth.user.email.split('@')
     const createComment = () => {
       if (comment != '') {
         save({ 
           content: comment,
           createdAt: firebase.database.ServerValue.TIMESTAMP,
           user: {
-            id: '1',
-            name: 'Rogério'
+            id: auth.user.uid,
+            name: displayName || alternativeDisplayName
           } 
         })
         setComment('')
