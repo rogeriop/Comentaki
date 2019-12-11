2ce5f47469fe366e9a37dc0c8064044b861f4d1e
rogeriop <pinheiroxrogerio@gmail.com>
2019-12-11 10:19:44 -0300

    Curso devReactJs #20.03 - Criando o projeto no Codeship e fazendo as primeiras configurações

--- src/Comment.js
+++ src/Comment.js
@@ -6,8 +6,8 @@ const Comment = ({ comment }) => {
     //const auth = useContext(AuthContext)
     return (
       <div>
-        
         <span class="text-black-50 border-bottom">{comment.content} por: {comment.user.name} em: <Time timestamp={comment.createdAt} /></span>
+        <p></p>
       </div>
     )
   }
